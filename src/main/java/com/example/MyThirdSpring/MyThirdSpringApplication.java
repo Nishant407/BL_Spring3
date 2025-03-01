2544cfcd57aaed054bfd86212e5d169f623c680a
Nishant407 <nishant407.ns@gmail.com>
2025-03-01 21:16:06 +0530

    UC2

--- src/main/java/com/example/MyThirdSpring/MyThirdSpringApplication.java
+++ src/main/java/com/example/MyThirdSpring/MyThirdSpringApplication.java
@@ -1,7 +1,7 @@
 package com.example.MyThirdSpring;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;;
 
 @SpringBootApplication
 public class MyThirdSpringApplication {
